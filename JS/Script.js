81e6134e9ae48b8695a02d7d726cf7d45dc56cef
ravi <ravisharma45935@gmail.com>
2022-04-15 12:44:14 -0700

    initial commit

---
+++ JS/Script.js
@@ -0,0 +1,25 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    cartItem.classList.remove('active');
+    searchIcon.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart--items-container');
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchIcon.classList.remove('active');
+}
+
+let searchIcon = document.querySelector('.search-box');
+document.querySelector('#search-btn').onclick = () =>{
+    searchIcon.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    searchIcon.classList.remove('active');
+}
